2718acb2bb1ff0a31f7312e1cb76db90c0942e94
Hugo Amorim <hugo@brandpendulum.com>
2015-06-09 14:35:58 -0400

    changes before clean

---
+++ js/Model.js
@@ -0,0 +1,55 @@
+// Top 10 Beer places in Portland 
+
+
+//https://www.google.com/maps/place/APEX/@45.5053448,-122.6528911,18z/data=!4m2!3m1!1s0x54950a77417c717b:0x7dbc1d5f87bd3906
+
+//https://www.google.com/maps/place/Bailey's+Taproom/@45.522543,-122.678169,17z/data=!3m1!4b1!4m2!3m1!1s0x54950a0157dd123d:0xd40f8d6a084d1937
+
+//https://www.google.com/maps/place/Bazi+Bierbrasserie/@45.511747,-122.632517,17z/data=!3m1!4b1!4m2!3m1!1s0x5495a0904b286df1:0x256721968fda560f
+
+//https://www.google.com/maps/place/The+BeerMongers/@45.505098,-122.654147,17z/data=!3m1!4b1!4m2!3m1!1s0x0000000000000000:0xbdb8193a8f04420e
+
+//https://www.google.com/maps/place/Belmont+Station/@45.5191,-122.615984,17z/data=!3m1!4b1!4m2!3m1!1s0x5495a0eeef65853d:0xd734a8e123c01149
+
+//https://www.google.com/maps/place/The+Hop+%26+Vine/@45.562629,-122.686864,17z/data=!3m1!4b1!4m2!3m1!1s0x5495a774eb22c06f:0x41e57bba5c19aed2
+
+//https://www.google.com/maps/place/Horse+Brass+Pub/@45.516411,-122.615528,17z/data=!3m1!4b1!4m2!3m1!1s0x5495a0ee0f31201b:0x2e9bdbed0325eede
+
+//https://www.google.com/maps/place/N.W.I.P.A./@45.489644,-122.597187,17z/data=!3m1!4b1!4m2!3m1!1s0x5495a014e9cfba0b:0xedb139f3941c2cb8
+
+//https://www.google.com/maps/place/Roscoe's/@45.519325,-122.58035,17z/data=!3m1!4b1!4m2!3m1!1s0x5495a106e1b961b5:0x6345545cfed7ee6e
+
+//https://www.google.com/maps/place/Saraveza/@45.562488,-122.677264,17z/data=!3m1!4b1!4m2!3m1!1s0x5495a70c388c482f:0xd94de585354bebb8
+
+
+/*
+ *	create knockout model to bind to the search element
+ */												
+function FourSquareBusinessViewModel() {
+	// set this to the self variable
+  var self = this;
+
+	/*
+	 *	set the bind-data to the search field to "Hawaiian Shaved Ice"
+	 *	observe for change
+	 */	
+	self.searchTerm = ko.observable('Hawaiian Shaved Ice');
+
+	/*
+	 *	function to update the view model
+	 */	
+	self.updateYelpResults = function(){
+		/*
+		 *	return the updated data from the search field
+		 *	then run the ajax function to create the yelp list
+		 */	
+		ko.computed(function(){
+			yelpAjax('96815', self.searchTerm());
+		}, self);
+	}	
+}
+
+/*
+ *	Start knockout dependency tracking
+ */	
+ko.applyBindings(new yelpBusinessViewModel());
